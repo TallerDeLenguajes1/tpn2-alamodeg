04d399b8087c37589c9111f82cb44ed3ab40163f
Alvaro <alvarodeguer@gmail.com>
2020-04-15 17:07:11 -0300

    P4 Listo

--- tp2_Punto3.cpp
+++ tp2_Punto3.cpp
@@ -1,20 +1,50 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-
-int aleatorio();
-
+#include <time.h>
+//PROTO//
+int Aleatorio(int min,int max);
+//
 #define FILAS 15
 
-int main(){
-    srand(time(NULL)); 
-    //int columnas = 5 + rand() % 14; //minimo + rand() % maximo-1;
-    //int Matrix[FILAS][aleatorio()];
-    //printf("%d",);
-    getchar();
+int main()
+{
+    srand(time(NULL));
+    int columnas = Aleatorio(5,15); //min + rand() % (max-min+1)
+    int **Matriz = (int **)malloc(FILAS * sizeof(int*)); //Vector de vectores - Asignacion dinamica de filas
+   //CREACION Y ASIGNACION DE LA MATRIZ
+    if (Matriz == NULL)
+    {
+        printf("Error al asignar filas");
+        exit(1);
+    }
+    for (int i = 0; i < FILAS; i++)
+    {
+        Matriz[i] = (int *)malloc(Aleatorio(5,15)*sizeof(int)); //Asignacion dinamica de las columnas
+        if (Matriz == NULL)
+        {
+            printf("Error al asignar filas");
+            exit(1);
+        }
+        for (int i = 0; i < FILAS; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                Matriz[i][j] = Aleatorio (100,999);
+            }
+            
+        }
+    }
+    //MUESTRA LA MATRIZ
+    for (int i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            printf("%d",Matriz[i][j]);
+        }
+    }
+    scanf(" %c");
     return 0;
 }
-int aleatorio(){
-    int aux = 5 + rand() % 14;
-    return aux;
+int Aleatorio(int min,int max){
+    return min + rand() % (max-min+1);
 }
